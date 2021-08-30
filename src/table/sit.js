5c578a9655bc3b5836798e94d22734503cc7ae6a
kanreisa <re@pixely.jp>
2021-08-30 12:17:59 +0900

    @1.3.5-mirakurun.10 - update napi-crc32 path

--- src/table/sit.js
+++ src/table/sit.js
@@ -1,5 +1,5 @@
 "use strict";
-const TsCrc32 = require("../../packages/napi-crc32");
+const TsCrc32 = require("@chinachu/napi-crc32");
 const TsReader = require("../reader");
 const TsDescriptors = require("../descriptors");
 
